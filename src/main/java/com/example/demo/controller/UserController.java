b81c2c46be9937d9a3f1b3bdcfa954bd4116b708
jiho shin <jhhj424@naver.com>
2021-06-05 23:03:14 +0900

    fix: User Create API Pah 수정

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,10 +4,7 @@ import com.example.demo.model.Header;
 import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,7 +13,7 @@ public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("user")
+    @PostMapping("")
     public Header<User> create(@RequestBody User requestUser) {
         User savedUser = userService.create(requestUser);
         if (savedUser != null) {
